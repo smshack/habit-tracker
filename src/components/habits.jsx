3960688f0394e61529de16680b50a7865c5dbccf
smshack <5432tat@naver.com>
2022-01-01 00:59:29 +0900

    navbar 추가

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,46 +2,21 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 3 },
-      { id: 2, name: 'Running', count: 0 },
-      { id: 3, name: 'Coding', count: 0 },
-    ],
-  };
-  handleIncrement = (habit) => {
-    console.log(`handleIncrement ${habit.name}`)
-    const habits =[...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({habits})
-  };
-  handleDecrement = (habit) => {
-    console.log(`handleDecrement ${habit.name}`)
-    const habits =[...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count-1;
-    habits[index].count = count < 0 ? 0:count;
-    this.setState({habits})
-  };
-  handleDelete = (habit) =>{
-    console.log(`handleDelete ${habit.name}`)
-    const habits = this.state.habits.filter(item => item.id !== habit.id)
-    this.setState({habits})
-  }
   render() {
     return (
-      <ul>
-        {this.state.habits.map(habit => (
-          <Habit 
-          key={habit.id} 
-          habit={habit} 
-          onIncrement={this.handleIncrement}
-          onDecrement={this.handleDecrement}
-          onDelete={this.handleDelete}
-          />
-        ))}
-      </ul>
+      <div className="habits">
+        <ul>
+          {this.props.habits.map(habit => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+      </div>
     );
   }
 }
